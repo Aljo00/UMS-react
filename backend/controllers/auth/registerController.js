7b2207e600f8e904f3258829dc17ee9c49cf905f
Aljo00 <aljojoju950@gmail.com>
2025-06-28 14:13:05 +0530

    feat: implement user authentication with registration and JWT token generation

---
+++ backend/controllers/auth/registerController.js
@@ -0,0 +1,39 @@
+import User from "../../models/User.js";
+import bcrypt from "bcryptjs";
+import generateToken from "../../utils/generateToken.js";
+
+const registerController = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    console.log(user);
+    console.log("This the res ", res)
+
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      image: user.image,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export default registerController;
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,20 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
+
+console.log("JWT_SECRET is:", process.env.JWT_SECRET);
 
 const app = express();
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 // Import your routes here (coming soon)
 
