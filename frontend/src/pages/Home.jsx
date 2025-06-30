9defe7a3381d7bb973d723c6474a9afb2c457152
Aljo00 <aljojoju950@gmail.com>
2025-06-30 21:36:12 +0530

    feat: add UpdateProfileModal component for user profile updates with image upload functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import UpdateProfileModal from "../components/UpdateProfileModal";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const { user } = useSelector((state) => state.auth);
+  const [showModal, setShowModal] = useState(false);
 
-export default Home
+  if (!user) return <p>Please log in</p>;
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Welcome, {user.name}</h2>
+      <p>Email: {user.email}</p>
+      {user.image && (
+        <img
+          src={user.image}
+          alt="Profile Photo"
+          style={{ width: "100px", borderRadius: "50%" }}
+        />
+      )}
+      <button onClick={() => setShowModal(true)}>Update the Profile</button>
+
+      {showModal && (
+        <UpdateProfileModal
+          user={user}
+          closeModal={() => setShowModal(false)}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Home;
